a1ba2c50e59b1ee2983dda2e9a25bdb084717c05
Koki Shinjo <sktometometo@gmail.com>
2020-05-26 10:49:46 +0900

    fix bugs

--- node_scripts/latched_topic_republisher.py
+++ node_scripts/latched_topic_republisher.py
@@ -7,24 +7,35 @@ import geometry_msgs.msg
 import tf2_ros
 import tf2_msgs.msg
 import sys
+import importlib
+import pydoc
 
 def main():
 
     rospy.init_node( 'latched_topic_republisher' )
 
-    topicname = rospy.get_param('~topicname','tf_static')
+    topicname = rospy.get_param('~topicname','/tf_static')
     bagfilename = rospy.get_param("~file")
 
-    with rosbag.Bag( bagfilename, 'r' ) as inputbag:
-        topic, msg, t = inputbag.read_message( topicname ).next()
-        packagename, typename = msg._type.split('/')
-        exec('from {}.msg import {}'.format(packagename,typename))
+    module = None
+    datatype = None
 
-    publisher = rospy.Publisher(topicname, typename, latch=True, queue_size=10)
+    with rosbag.Bag( bagfilename, 'r' ) as inputbag:
+        try:
+            topic, msg, t = inputbag.read_messages( topicname ).next()
+        except:
+            rospy.logerror( 'Cannot find any message with topic {}.'.format( topicname ) )
+            sys.exit(1)
+        packagename, typename = msg._type.split('/')
+        print('from {}.msg import {}'.format(packagename,typename))
+        module = importlib.import_module('{}.msg'.format(packagename))
+        datatype = pydoc.locate('{}.msg.{}'.format(packagename,typename))
+
+    publisher = rospy.Publisher(topicname, datatype, latch=True, queue_size=10)
 
     list_messages = []
     with rosbag.Bag( bagfilename, 'r' ) as inputbag:
-        for topic, msg, t in inputbag.read_messages('/tf_static'):
+        for topic, msg, t in inputbag.read_messages( topicname ):
             list_messages.append(msg)
 
     for message in list_messages:
